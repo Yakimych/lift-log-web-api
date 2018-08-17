36cf9a5d1e6289c31260fb6acfb4d92fbd23882e
Kyrylo Yakymenko <yakimych@gmail.com>
2018-08-17 15:16:17 +0200

    Add OptionConverter

--- src/LiftLog.WebApi/Startup.fs
+++ src/LiftLog.WebApi/Startup.fs
@@ -6,16 +6,21 @@ open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Swashbuckle.AspNetCore.Swagger
+open LiftLog.WebApi.Converters
 
 type Startup private () =
     new (configuration: IConfiguration) as this =
         Startup() then
         this.Configuration <- configuration
-
+        
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
-        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1) |> ignore
+        services.
+            AddMvc().
+            AddJsonOptions(fun options -> options.SerializerSettings.Converters.Add(new OptionConverter())).
+            SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            |> ignore
         services.AddSwaggerGen(fun c -> c.SwaggerDoc("v1", new Info(Title = "Lifts API", Version = "v1"))) |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
