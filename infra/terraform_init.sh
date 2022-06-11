d0ceb11b6359c4cbf36f2136a0b35f6308014eac
Kyrylo Yakymenko <kya@combination.se>
2022-06-11 20:15:07 +0200

    Print account key

--- infra/terraform_init.sh
+++ infra/terraform_init.sh
@@ -4,6 +4,9 @@ RESOURCE_GROUP_NAME=tfstate
 STORAGE_ACCOUNT_NAME=tfstateliftlog
 
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+
+echo $ACCOUNT_KEY
+
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
 
 terraform init
